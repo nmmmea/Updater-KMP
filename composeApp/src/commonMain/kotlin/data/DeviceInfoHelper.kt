d3afc14cade40120f508fc349f54da1b439b203d
nmmmea <95582630+nmmmea@users.noreply.github.com>
2025-08-21 19:13:25 +0800

    Update DeviceInfoHelper.kt

--- composeApp/src/commonMain/kotlin/data/DeviceInfoHelper.kt
+++ composeApp/src/commonMain/kotlin/data/DeviceInfoHelper.kt
@@ -211,6 +211,7 @@ object DeviceInfoHelper {
         Device("REDMI 15C / POCO C85", "dew", "BN"),
         Device("REDMI 15", "creek", "BO"),
         Device("REDMI 15 5G / POCO M7 Plus", "spring", "OU"),
+        Device("REDMI Note 15 Pro+", "flourite", "PR"),
     )
 
     private val androidW = Android("16.0", "W")
